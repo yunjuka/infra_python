dc73be0b91e254c5e7344d8a44d6dd6b860b8c37
kangyunju <lio464628@gmail.com>
2026-03-18 12:28:20 +0900

    파일 세개 추가

---
+++ python_basic/Step01_DataType.py
@@ -0,0 +1,36 @@
+
+print("step구문시작")
+
+num1 = 10
+num2 = 10.1
+
+Name1 = '김구라'
+Name2 = '해골'
+Name3 = """
+    KT cloud Infra
+    """
+
+foods = ['삼겹살', '김밥', '닭발']
+
+[print(foods)]
+
+mem1 = {'num':1, 'age':24, 'addr':"노량진"} #dict 타입 = dictionary
+#dictionary {"key":value}
+print(mem1)
+
+mySet = {"노랑","삘깅","파랑"} #set 타입
+print(mySet)
+
+def store():
+    print("연다")
+    print("저장")
+    print("닫기")
+    print("끝")
+
+store()
+returnValue = store()
+
+a = None
+print("작업")
+a = "test"
+b = 7
---
+++ python_basic/Step02_Dict.py
@@ -0,0 +1,19 @@
+
+mem1 = {"num":1, "name":"kimgura", "isman":True}
+
+#print(mem1.num)
+print(mem1["num"])
+print(mem1["name"])
+print(mem1["isman"])
+
+a = mem1["num"]
+b = mem1['name']
+c = mem1['isman']
+
+# dict 내용 수정하기
+mem1["num"] = 2
+
+mem2 = {"num":3, "name":"해골", "isman":True}
+
+print("종료")
+
